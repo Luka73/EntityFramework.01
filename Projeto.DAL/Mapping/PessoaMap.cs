ae74abad6611a60135b02b78b4144c7a3c6e9dcb
Luka73 <luanafernandes73@gmail.com>
2019-11-09 10:36:58 -0300

    Creating base

---
+++ Projeto.DAL/Mapping/PessoaMap.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity.ModelConfiguration;
+using Projeto.DAL.Entities;
+
+namespace Projeto.DAL.Mapping
+{
+    //REGRA 1) Herdar a classe EntityTypeConfiguration
+    public class PessoaMap : EntityTypeConfiguration<Pessoa>
+    {
+        public PessoaMap()
+        {
+
+        }
+    }
+}
---
+++ Projeto.Service/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Projeto.Service.WebApiApplication" Language="C#" %>
